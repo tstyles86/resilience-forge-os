0cf97615c917f596516f64aa52eaa1c39ec2e22a
tstyles86 <tstyles86@gmail.com>
2026-06-01 01:13:40 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import TravisAIOS from "./TravisAIOS.jsx";
+createRoot(document.getElementById("root")).render(<React.StrictMode><TravisAIOS /></React.StrictMode>);
